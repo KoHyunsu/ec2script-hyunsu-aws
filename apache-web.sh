f5d47cd9ec375c1e100ea82a6634ce5deb93bfd7
KoHyunsu <39062454+KoHyunsu@users.noreply.github.com>
2023-01-16 07:36:24 +0900

    [Add] apache-web.sh

---
+++ apache-web.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -xe
+# /var/log/user-data.log에 에러를 기록하겠다
+exec > >(sudo tee /var/log/user-data.log|sudo sh -c "logger -t user-data -s 2>/dev/console") 2>&1
+
+# 설치 전에 우선 업데이트부터 하겠다
+sudo yum update -y
+
+# Apache(httpd) 설치하겠다
+sudo yum install httpd -y
+
+# Apache(httpd) 시작하겠다
+sudo service httpd start
+sudo chkconfig httpd on
+
+# /var/www/html 위치로 이동
+cd /var/www/html
+
+# 기본 페이지(index.html) 만들겠다
+sudo echo "Index.hsudotml AWS EC2. $(hostname -f)" > index.html
+
+# /hello 페이지 만들겠다
+sudo echo "Hello AWS EC2. $(hostname -f)" > hello
+
+# /what 페이지 만들겠다
+sudo echo "What AWS EC2. $(hostname -f)" > what
